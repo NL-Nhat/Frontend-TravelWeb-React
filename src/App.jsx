52d7a8fd08fd4414713a4a24b25425e1411463c9
NL-Nhat <nguyenlongnhat20050111@gmail.com>
2026-03-19 20:13:14 +0700

    page register

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Tours from './pages/Tours';
 import TourDetail from './pages/TourDetail';
 import Login from './pages/Login';
+import Register from './pages/Register ';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/tours" element={<Tours />} />
       <Route path="/tour-detail/:id" element={<TourDetail />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
